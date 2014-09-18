52d72440856f771feab86286d9fb70624113f6ef
Joshua Kemp <josh.kemp@ziplist.com>
2014-09-18 14:51:15 -0400

    Genereating the basic app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FarrierAppTest::Application.config.secret_token = 'a1dc44ad0c6e7bcfd9a54a0ecd49e8f45c5f5246af08831e616e8d082921fe36ad2c7f7931eff9021fc40247fe385338571e45ab954a3edb9a8628bfecf70ddd'
